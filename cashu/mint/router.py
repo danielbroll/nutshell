4aa1b5ef6322b67e1e9f314ea6d31691b465fd56
danielbroll <danielbroll@protonmail.com>
2025-12-10 20:17:17 +0100

    Update version string to "third version"

--- cashu/mint/router.py
+++ cashu/mint/router.py
@@ -509,5 +509,5 @@ async def get_statistics() -> StatisticsResponse:
         total_mints=mint_count,
         total_melts=melt_count,
         total_swaps=swap_count,
-        version="second version"
+        version="third version"
     )
